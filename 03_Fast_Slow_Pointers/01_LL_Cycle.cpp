232d972f8d5788cc645472d0624741d91ab2408b
kirankumar.g1 <kirankumar.g@sanas.ai>
2025-08-22 22:49:22 +0530

    Fast Slow Pointers - Prob - 1

---
+++ 03_Fast_Slow_Pointers/01_LL_Cycle.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/linked-list-cycle/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *slow = head;
+        ListNode *fast = head;
+
+        while (slow && fast && fast -> next) {
+            slow = slow -> next;
+            fast = fast -> next -> next;
+        
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+// Time -> O(N)
+// Space -> O(1)

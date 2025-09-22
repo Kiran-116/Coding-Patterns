5d50df0982624be375ce03d1acfeb1f73bc667d0
kirankumar.g1 <kirankumar.g@sanas.ai>
2025-09-22 22:01:17 +0530

    Tree BFS - Prob - 3

---
+++ 07_Tree_BFS/03_BT_Zigzag_Level_Order_Traversal.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if (!root) {
+            return ans;
+        }
+        bool LtoROrder = true;
+        queue<TreeNode *> q;
+        q.push(root);
+        while (!q.empty()) {
+            int size = q.size();
+            vector<int> temp;
+            for (int i = 0 ; i < size ; i++) {
+                TreeNode *node = q.front();
+                q.pop();
+                temp.push_back(node -> val);
+                if (node -> left) {
+                    q.push(node -> left);
+                }
+                if (node -> right) {
+                    q.push(node -> right);
+                }
+            }
+            if (!LtoROrder) {
+                reverse(temp.begin(), temp.end());
+            }
+            ans.push_back(temp);
+            LtoROrder = !LtoROrder;
+        }
+        return ans;
+    }
+};
+// Time -> O(N)
+// Space -> O(N)
